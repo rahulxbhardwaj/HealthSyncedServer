d2d2467b478be769c64cff6191aea54956951681
Rahul Bhardwaj <147973917+rahulxbhardwaj@users.noreply.github.com>
2024-04-10 03:07:37 +0000

    added mongo db atlas database

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 const express = require('express');
 const server = express();
 const cors = require('cors');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+
+const DB_URL = 'mongodb+srv://rahulxbhardwaj:Rahul98112%40@cluster0.lnoim7o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+});
+
+
+mongoose.connect(DB_URL,).then(() => {
+  console.log('Connected to MongoDB ! ');
+})
+
+const User = mongoose.model('User', userSchema);
+
 
 server.use(cors());
 server.use(bodyParser.json());
 
-server.post('/demo' ,(req,res)=> {
-  console.log(req.body);
-  res.json(req.body);
+server.post('/demo' ,async(req,res)=> {
+
+  let user = new User();
+  User.name = req.body.name;
+  User.age = req.body.age;
+  const doc = await user.save();
+  
+  console.log(doc);
+  res.json(doc);
 })
 
 
