c06ac6573c5de97719f28fb4a44cfd75c5b7a8c1
Rahul Bhardwaj <147973917+rahulxbhardwaj@users.noreply.github.com>
2024-04-11 06:08:35 +0000

    added Mobile no , bmi and other feilds

--- index.js
+++ index.js
@@ -4,51 +4,57 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+// Environment Variables
+const DB_URL = process.env.DB_URL;
 
-//Environment Variables
-const DB_URL = process.env['DB_URL'];
-
-
-//Mongoose Schema
+// Mongoose Schema
 const userSchema = new mongoose.Schema({
   name: String,
   age: Number,
+  bmi: Number,
+  mobile: Number,
+  history: String,
 });
 
-
-mongoose.connect(DB_URL,).then(() => {
-  console.log('Connected to MongoDB ! ');
-})
+mongoose.connect(DB_URL).then(() => {
+  console.log('Connected to MongoDB!');
+});
 
 const User = mongoose.model('User', userSchema);
-
 
 server.use(cors());
 server.use(bodyParser.json());
 
+server.post('/newUser', async (req, res) => {
+  let user = new User({
+    name: req.body.name,
+    age: req.body.age,
+    bmi: req.body.bmi,
+    mobile: req.body.number,
+    history: req.body.history,
+  });
 
-server.post('/newUser' ,async(req,res)=> {
-
-  let user = new User();
-  user.name = req.body.name;
-  user.age = req.body.age;
-  const doc = await user.save();
-  
-  console.log(doc);
-  res.json(doc);
-})
-
-server.get('/alluser' , async(req,res)=>{
-   try {
-     const allUsers = await User.find(); 
-     res.json(allUsers); 
-   } catch (error) {
-     console.error('Error fetching all users:', error);
-     res.status(500).json({ error: 'Internal Server Error' });
-   }
-})
+  try {
+    const doc = await user.save();
+    console.log(doc);
+    res.json(doc);
+  } catch (error) {
+    console.error('Error saving user:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 
-server.listen(8080 , () => {
-  console.log("Server Started !")
-})
+server.get('/alluser', async (req, res) => {
+  try {
+    const allUsers = await User.find();
+    res.json(allUsers);
+  } catch (error) {
+    console.error('Error fetching all users:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+server.listen(8080, () => {
+  console.log('Server Started!');
+});
